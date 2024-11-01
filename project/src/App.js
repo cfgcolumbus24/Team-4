efa096ea7e8712a3e620ea0692d97666caa61ff9
Mariana Ramos-Andujar <ramos-andujar.1@buckeyemail.osu.edu>
2024-11-01 19:20:21 -0400

    Pushed changes to React.

--- project/src/App.js
+++ project/src/App.js
@@ -1,25 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './Login';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        {/* This will make the Login component load first as the default route */}
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+
