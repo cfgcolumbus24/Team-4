4e6b4e96d9f26e4b7841910281724efca07d5e25
anvita-g <anvigollu@gmail.com>
2024-11-02 00:51:28 -0400

    rendering volunteers from firebase

--- project/src/App.js
+++ project/src/App.js
@@ -2,16 +2,17 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RoleSelection from './Signup';
 import LoginPage from './Login';
+import VolunteerForm from './VolunteerForm';
 
 function App() {
   return (
     <Router>
       <Routes>
-      <Route path="/" element={<LoginPage />} />
+        <Route path="/" element={<LoginPage />} />
         <Route path="/signup" element={<RoleSelection />} />
+        <Route path="/volunteer" element={<VolunteerForm />} /> {/* Add this line for the VolunteerForm */}
       </Routes>
     </Router>
   );
 }
-
 export default App;
